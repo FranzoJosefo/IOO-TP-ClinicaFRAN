da62c998a4ff5646b5f51c025226d772a15c8b81
manaraujo <manuel.araujo@mercadolibre.com>
2020-04-06 00:40:55 -0300

    First classes

--- src/clases/Paciente.java
+++ src/clases/Paciente.java
@@ -1,5 +1,24 @@
 package clases;
 
-public class Paciente {
+public class Paciente extends Persona {
 
+	private Sexo sexo;
+	private Edad edad;
+	
+	public Sexo getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+	
+	public Edad getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(Edad edad) {
+		this.edad = edad;
+	}
+	
 }
