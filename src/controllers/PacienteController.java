37230a047fb16d6b31b5e64074a5ba6fd459e47c
FranzoJosefo <franj.olivero@gmail.com>
2020-06-09 23:24:04 -0300

    agrego gson ServiceAPI para leer y grabar cambios con ejemplo en PacienteController

--- src/controllers/PacienteController.java
+++ src/controllers/PacienteController.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import data.ApiService;
 import dtos.DireccionDTO;
 import dtos.PacienteDTO;
 import entities.Direccion;
@@ -13,42 +14,64 @@ import utils.CodigoGenerator;
 public enum PacienteController {
 
 	INSTANCE;
-	
+
 	private int pacientesCreados;
 	private List<Paciente> pacientes;
-	
+
 	PacienteController() {
 		pacientesCreados = 0;
 		pacientes = new ArrayList();
+		fetchPacientesJson();
 	}
-	
+
 	public void createPaciente(PacienteDTO pacienteDto) {
 		pacienteDto.setCodigo(generateCodigoPaciente());
 		Paciente newPaciente = new Paciente(pacienteDto);
-		pacientes.add(newPaciente);		
+		pacientes.add(newPaciente);
+		updatePacientesJson();
 	}
-	
+
+	private void fetchPacientesJson() {
+		String file = "./pacientes.txt";
+		List<PacienteDTO> dtos = ApiService.leer(PacienteDTO.class, file);
+		for (PacienteDTO dto : dtos) {
+			pacientes.add(new Paciente(dto));
+		}
+	}
+
+	private void updatePacientesJson() {
+		String file = "./pacientes.txt";
+		
+		ApiService.grabar(getPacientesDTO(), file);
+	}
+
 	public Paciente getPaciente(String codigoPaciente) throws Exception {
-		return pacientes.stream()
-		.filter(p -> p.getCodigo().equals(codigoPaciente))
-		.findFirst()
-		.orElseThrow(() -> new Exception("No se ha encontrado el paciente"));
+		return pacientes.stream().filter(p -> p.getCodigo().equals(codigoPaciente)).findFirst()
+				.orElseThrow(() -> new Exception("No se ha encontrado el paciente"));
 	}
 	
+	public List<PacienteDTO> getPacientesDTO() {
+		List<PacienteDTO> dtos = new ArrayList<PacienteDTO>();
+		for (Paciente paciente : pacientes) {
+			dtos.add(paciente.toDto());
+		}
+		return dtos;
+	}
+
 	public void deletePaciente(String codigoPaciente) throws Exception {
-		if(PeticionController.INSTANCE.hasPeticionesWithEstudiosTerminados(codigoPaciente)) {
+		if (PeticionController.INSTANCE.hasPeticionesWithEstudiosTerminados(codigoPaciente)) {
 			throw new Exception("No se puede eliminar al paciente porque tiene estudios finalizados.");
 		}
 		pacientes.removeIf(p -> p.getCodigo().equals(codigoPaciente));
 	}
-	
+
 	public Direccion createDireccion(DireccionDTO direccionDto) {
 		return new Direccion(direccionDto);
 	}
-	
+
 	private String generateCodigoPaciente() {
 		pacientesCreados++;
 		return CodigoGenerator.generateCodigo(PrefijoCodigo.PACIENTE, pacientesCreados);
 	}
-	
+
 }
